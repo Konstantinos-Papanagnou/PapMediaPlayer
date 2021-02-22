fb2be6c55f442dd2ecb5c1fde8f8543cce4a5cd7
Konstantinos Pap <63195954+Konstantinos-Papanagnou@users.noreply.github.com>
2021-02-22 18:58:54 +0200

    Updates?

---
+++ PapMediaPlayer/ViewPlayManager.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.Media;
+using PapMediaPlayer.Models;
+using PapMediaPlayer.Adapters;
+
+namespace PapMediaPlayer
+{
+    public enum ViewMethods
+    {
+        AllTracks,
+        ContainingFolders,
+        Playlist,
+        Selection,
+        ContainingFolderSongs
+    }
+
+    public enum RepeatMethod
+    {
+        RepeatOne,
+        RepeatAllInOrder,
+        NoRepeat,
+        RepeatOnce,
+        RandomReplay
+    }
+
+    public class ViewMethodEventArgs
+    {
+        public ViewMethods newVMethod { get; private set; }
+        public ViewMethodEventArgs(ViewMethods VMethod)
+        {
+            newVMethod = VMethod;
+        }
+    }
+
+    public class RepeatMethodEventArgs:EventArgs
+    {
+        public RepeatMethod newRMethod { get; private set; }
+        public RepeatMethodEventArgs(RepeatMethod RMethod)
+        {
+            newRMethod = RMethod;
+        }
+    }
+
+    public class ViewPlayManager
+    {
+
+        public bool reachedEnd = false;
+        private int prevPos = 0;
+
+        public delegate void ViewMethodChanged(object sender, ViewMethodEventArgs e);
+        public event ViewMethodChanged OnViewMethodChanged;
+
+        public delegate void RepeatMethodChanged(object sender, RepeatMethodEventArgs e);
+        public event RepeatMethodChanged OnRepeatMethodChanged;
+
+        private RepeatMethod rep;
+        private ViewMethods viewm;
+
+        public RepeatMethod Repeat
+        {
+            get { return rep; }
+            set
+            {
+                rep = value;
+                if (OnRepeatMethodChanged == null)
+                    return;
+                RepeatMethodEventArgs args = new RepeatMethodEventArgs(value);
+                OnRepeatMethodChanged(this, args);
+            }
+        }
+
+        public ViewMethods ViewMethod
+        {
+            get { return viewm; }
+            set
+            {
+                viewm = value;
+                if (OnViewMethodChanged == null)
+                    return;
+                ViewMethodEventArgs args = new ViewMethodEventArgs(value);
+                OnViewMethodChanged(this, args);
+            }
+        }
+
+        #region Constructors
+        /// <summary>
+        /// Creates a new instance of ViewPlayManager with default values
+        /// Default Values: ViewMethod -> AllTracks
+        /// RepeatMode -> RepeatAllInOrder
+        /// </summary>
+        public ViewPlayManager() : this(ViewMethods.AllTracks, RepeatMethod.RepeatAllInOrder)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new instance of ViewPlayManager with specified viewMethod and default RepeatMode
+        /// Default Value: RepeatMode -> RepeatAllInOrder
+        /// </summary>
+        /// <param name="viewMethod">View Method style</param>
+        public ViewPlayManager(ViewMethods viewMethod) : this(viewMethod, RepeatMethod.RepeatAllInOrder)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new instance of ViewPlayManager with specified RepeatMethod and default ViewMethod
+        /// Default Value: ViewMethod -> AllTracks
+        /// </summary>
+        /// <param name="RepMethod">Repeat Method style</param>
+        public ViewPlayManager(RepeatMethod RepMethod) : this(ViewMethods.AllTracks, RepMethod)
+        {
+        }
+
+        /// <summary>
+        /// Creates a new instance of ViewPlayManager
+        /// </summary>
+        /// <param name="viewMethod">View Method style</param>
+        /// <param name="RepMethod">Repeat Method style</param>
+        public ViewPlayManager(ViewMethods viewMethod, RepeatMethod RepMethod)
+        {
+            viewm = viewMethod;
+            rep = RepMethod;
+        }
+        #endregion
+
+        /// <summary>
+        /// Handles repeats and returns the new position to go and seek in the list
+        /// </summary>
+        /// <param name="max">Max of the list</param>
+        /// <param name="position">The position in the tracks list</param>
+        /// <returns>The new position</returns>
+        public int HandleAutoRepeat(int max, int position)
+        {
+            switch (Repeat)
+            {
+                case RepeatMethod.RandomReplay:
+                    Random rand = new Random(DateTime.Now.Millisecond);
+                    prevPos = position;
+                    int value = rand.Next(0, max);
+                    if (value == position && max > 8)
+                        value = HandleAutoRepeat(max, position);
+                    return value;
+                case RepeatMethod.RepeatOne:
+                    if (position > max - 1)
+                        return 0;
+                    return position;
+                case RepeatMethod.NoRepeat:
+                    return position;
+                case RepeatMethod.RepeatOnce:
+                    if (position < max - 1)
+                    {
+                        reachedEnd = false;
+                        return ++position;
+                    }
+                    reachedEnd = true;
+                    return position;
+                default:
+                    if (position >= max - 1)
+                        return 0;
+                    return ++position;
+            }
+        }
+
+        /// <summary>
+        /// Handles user activity
+        /// </summary>
+        /// <param name="max">Max of the list</param>
+        /// <param name="position">The position in the tracks list</param>
+        /// <returns>The new position</returns>
+        public int HandleUserActivity(int max, int position, bool backwards = false)
+        {
+            if (Repeat == RepeatMethod.RandomReplay)
+            {
+                if (backwards && position != prevPos && prevPos < max)
+                    return prevPos;            
+                Random rand = new Random(DateTime.Now.Millisecond);
+                int value = rand.Next(0, max);
+                if (value == position && max > 8)
+                {
+                    value = HandleUserActivity(max, position);
+                }
+                prevPos = position;           
+                return value;
+            }
+            else
+            {
+                if (backwards)
+                {
+                    if (position <= 0)
+                        return max - 1;
+                    return --position;
+                }
+                if (position >= max - 1)
+                    return 0;
+                return ++position;
+            }
+        }
+        /// <summary>
+        /// Sets the adapter accordingly based on the ViewMethod enumeration
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="view"></param>
+        /// <param name="tracks"></param>
+        /// <param name="contFolders"></param>
+        public void SetAdapter(Context context, ref GridView view, List<Track> tracks, List<string> contFolders)
+        {
+            switch (ViewMethod)
+            {
+                case ViewMethods.AllTracks:
+                    view.Adapter = new SongAdapter(context, tracks);
+                    break;
+                case ViewMethods.ContainingFolders:
+                    view.Adapter = new ContainingFolderAdapter(context, contFolders);
+                    break;
+                case ViewMethods.Playlist:
+                    view.Adapter = new SongAdapter(context, tracks);
+                    break;
+                case ViewMethods.Selection:
+                    view.Adapter = new SongMultiSelectAdapter(context, ref MainActivity.selectedItems);
+                    break;
+                case ViewMethods.ContainingFolderSongs:
+                    view.Adapter = new SongAdapter(context, tracks);
+                    break;
+            }
+        }
+    }
+}
---
+++ PapMediaPlayer/XmlParser/XmlCacheParser.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System.Xml.Linq;
+using PapMediaPlayer.Models;
+using Android.Graphics;
+using System.IO;
+using PapMediaPlayer.StorageHelper;
+using TagFile = TagLib.File;
+using System.Threading.Tasks;
+using System.Security;
+
+namespace PapMediaPlayer.XmlParser
+{        
+    public sealed class XmlCacheParser: IDisposable
+    {
+        XDocument doc;
+
+        public XmlCacheParser()
+        {
+            if (File.Exists(System.IO.Path.Combine(InternalStorageHelper.InternalXmlFileLocation, "Cache", "Cache.xml")))
+                doc = XDocument.Load(System.IO.Path.Combine(InternalStorageHelper.InternalXmlFileLocation, "Cache", "Cache.xml"));
+        }
+        public XmlCacheParser(List<Track> tracks)
+        {
+            Directory.CreateDirectory(System.IO.Path.Combine(InternalStorageHelper.InternalXmlFileLocation, "Cache"));
+            if (File.Exists(System.IO.Path.Combine(InternalStorageHelper.InternalXmlFileLocation, "Cache", "Cache.xml")))
+                File.Delete(System.IO.Path.Combine(InternalStorageHelper.InternalXmlFileLocation, "Cache", "Cache.xml"));
+            doc = new XDocument(new XElement("Root"));
+            foreach(Track t in tracks)
+            {
+                string filePath;
+                if (t.ContainsImage)
+                {
+                    filePath = t.Path;
+                }
+                else filePath = "Default";
+                
+
+                XElement element = new XElement("Track", filePath);
+
+                element.Add(new XAttribute("Title", t.FullTitle));
+                element.Add(new XAttribute("Name", t.Name));
+                element.Add(new XAttribute("Year", t.Year));
+                element.Add(new XAttribute("Artist", t.AuthorName));
+                element.Add(new XAttribute("Album", t.Album));
+                element.Add(new XAttribute("Path", t.Path));
+                element.Add(new XAttribute("ContainingFolder", t.ContainingFolderName));
+                element.Add(new XAttribute("Copyrights", t.Copyrights));
+                element.Add(new XAttribute("Length", t.Length));
+                element.Add(new XAttribute("DurationMins", t.DurationMins));
+                element.Add(new XAttribute("IsCorrupted", t.isCorrupted));
+                element.Add(new XAttribute("CorruptionReason", t.CorruptionReason == null? "": t.CorruptionReason.ToString()));
+                element.Add(new XAttribute("Genre", t.Genre));
+                doc.Root.Add(element);
+            }
+            doc.Save(System.IO.Path.Combine(InternalStorageHelper.InternalXmlFileLocation, "Cache", "Cache.xml"));
+        }
+
+        public static XDocument GetDoc()
+        {
+            return XDocument.Load(System.IO.Path.Combine(InternalStorageHelper.InternalXmlFileLocation, "Cache", "Cache.xml"));
+        }
+
+        public static List<Track> GetCachedSongs(XDocument doc)
+        {
+            List<Track> tracks = new List<Track>();
+
+            foreach (XElement element in doc.Root.Elements())
+            {
+                tracks.Add(new Models.Track
+                {
+                    FullTitle = element.Attribute("Title").Value,
+                    Name = element.Attribute("Name").Value,
+                    AuthorName = element.Attribute("Artist").Value,
+                    Album = element.Attribute("Album").Value,
+                    Path = element.Attribute("Path").Value,
+                    ContainingFolderName = element.Attribute("ContainingFolder").Value,
+                    Copyrights = element.Attribute("Copyrights").Value,
+                    Length = element.Attribute("Length").Value,
+                    DurationMins = CastToDouble(element.Attribute("DurationMins").Value),
+                    isCorrupted = Convert.ToBoolean(element.Attribute("IsCorrupted").Value),
+                    Genre = element.Attribute("Genre").Value,
+                    Year = Convert.ToUInt32(element.Attribute("Year").Value)
+                });
+            }
+            return tracks;
+        }
+        [SecuritySafeCritical]
+        public List<Track> GetCachedSongs(Context c)
+        {
+            List<Track> tracks = new List<Track>();
+
+            foreach(XElement element in doc.Root.Elements())
+            {
+                bool hasImage = false;
+                Bitmap image = null;
+                if (element.Value != "Default")
+                {
+                    hasImage = true;
+                    using (TagFile mp3file = TagFile.Create(element.Value))
+                    {
+                        var bin = (byte[])(mp3file.Tag.Pictures[0].Data.Data);
+                        try
+                        {
+                            using (BitmapFactory.Options opt = new BitmapFactory.Options())
+                            {
+                                opt.OutHeight = 48;
+                                opt.OutWidth = 48;
+                                opt.InSampleSize = 2;
+                                image = BitmapFactory.DecodeByteArray(bin, 0, bin.Length, opt);
+                            }
+                        }
+                        catch (Java.Lang.OutOfMemoryError)
+                        {
+                            hasImage = false;
+                            throw new OutOfMemoryException("Unable to load image due to extensive memory usage. Please restart the application");
+                        }
+                    }
+            }
+                tracks.Add(new Models.Track
+                {
+                    FullTitle = element.Attribute("Title").Value,
+                    Name = element.Attribute("Name").Value,
+                    AuthorName = element.Attribute("Artist").Value,
+                    Album = element.Attribute("Album").Value,
+                    Path = element.Attribute("Path").Value,
+                    ContainingFolderName = element.Attribute("ContainingFolder").Value,
+                    Copyrights = element.Attribute("Copyrights").Value,
+                    Length = element.Attribute("Length").Value,
+                    DurationMins = CastToDouble(element.Attribute("DurationMins").Value),
+                    isCorrupted = Convert.ToBoolean(element.Attribute("IsCorrupted").Value),
+                    Genre = element.Attribute("Genre").Value,
+                    Year = Convert.ToUInt32(element.Attribute("Year").Value),
+                    Image = image,
+                    ContainsImage = hasImage
+                });
+            }
+            return tracks;
+        }
+        public int GetMaxCountOfElements()
+        {
+            return doc.Root.Elements().Count();
+        }
+        [SecuritySafeCritical]
+        public Track GetCachedSong(Context c, int position)
+        {
+           
+                bool hasImage = false;
+                Bitmap image = null;
+
+            XElement element = doc.Root.Elements().ElementAt(position);
+                if (element.Value != "Default")
+                {
+                    hasImage = true;
+                using (TagFile mp3file = TagFile.Create(element.Value))
+                {
+                    var bin = (byte[])(mp3file.Tag.Pictures[0].Data.Data);
+                    try
+                    {
+                        using (BitmapFactory.Options opt = new BitmapFactory.Options())
+                        {
+                            opt.OutHeight = 48;
+                            opt.OutWidth = 48;
+                            opt.InSampleSize = 2;
+                            image = BitmapFactory.DecodeByteArray(bin, 0, bin.Length, opt);
+                        }
+                    }
+                    catch (Java.Lang.OutOfMemoryError)
+                    {
+                        hasImage = false;
+                        throw new OutOfMemoryException("Unable to load image due to extensive memory usage. Please restart the application.");
+                    }
+                }
+            }
+
+                return new Track
+                {
+                    FullTitle = element.Attribute("Title").Value,
+                    Name = element.Attribute("Name").Value,
+                    AuthorName = element.Attribute("Artist").Value,
+                    Album = element.Attribute("Album").Value,
+                    Path = element.Attribute("Path").Value,
+                    ContainingFolderName = element.Attribute("ContainingFolder").Value,
+                    Copyrights = element.Attribute("Copyrights").Value,
+                    Length = element.Attribute("Length").Value,
+                    DurationMins = CastToDouble(element.Attribute("DurationMins").Value),
+                    isCorrupted = Convert.ToBoolean(element.Attribute("IsCorrupted").Value),
+                    Genre = element.Attribute("Genre").Value,
+                    Year = Convert.ToUInt32(element.Attribute("Year").Value),
+                    Image = image,
+                    ContainsImage = hasImage
+                };
+        }
+
+        private static double CastToDouble(string stringToSplit)
+        {
+            string[] array = stringToSplit.Split('.');
+            string Final;
+            if (array.Length == 2)
+            {
+                Final = array[0] + "." + array[1];
+                string ToCompare = array[0] + array[1];
+                if (double.Parse(Final) == double.Parse(ToCompare))
+                {
+                    Final = array[0] + "," + array[1];
+                    return double.Parse(Final);
+                }
+                else
+                {
+                    return double.Parse(Final);
+                }
+            }
+            else
+            {
+                Final = array[0];
+                return double.Parse(Final);
+            }
+        }
+
+        //Release associated resources and delete cache
+        public void Dispose()
+        {
+            doc = null;
+            File.Delete(System.IO.Path.Combine(InternalStorageHelper.InternalXmlFileLocation, "Cache", "Cache.xml"));
+            GC.SuppressFinalize(this);
+        }
+
+        //Release associated resources.
+        public void Release()
+        {
+            GC.SuppressFinalize(doc);
+            doc = null;
+        }
+    }
+}
